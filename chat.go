11692d2e52fb1b9fb14f8d9b026cfdae85306cfd
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2024-12-15 18:10:21 +0100

    x

--- chat.go
+++ chat.go
@@ -55,7 +55,7 @@ func newChatServer() *chatServer {
 	cs.serveMux.HandleFunc("/subscribe", cs.subscribeHandler)
 	// cs.serveMux.HandleFunc("/publish", cs.publishHandler)
 
-	go cs.MonitorFile("192.168.2.104", "/home/test/mnt/test/data/data/data.json")
+	go cs.MonitorFile("192.168.2.104", "/home/test/andros/data/data/data.json")
 
 	return cs
 }
