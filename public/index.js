cec0de97739504f15f565cd56fe1fc8a07406132
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-01-13 21:58:06 +0100

    x

--- public/index.js
+++ public/index.js
@@ -8,6 +8,43 @@
   var n
   var map
 
+  function sortTable(table) {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById(table);
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("th")[0].split(".");
+        x = parseInt([x.length - 1])
+        y = rows[i + 1].getElementsByTagName("th")[0];
+        y = parseInt([y.length - 1])
+        // Check if the two rows should switch place:
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+  }
+
   function dial() {
     console.log(location.host)
     const conn = new WebSocket(`ws://${location.host}/andros/subscribe`)
@@ -180,6 +217,8 @@
           <th scope="col">${data.ina.bus_voltage}</th>
           <th scope="col">${data.ina.power}</th>
         `
+        sortTable("status")
+        sortTable("data")
       }
     })
   }
