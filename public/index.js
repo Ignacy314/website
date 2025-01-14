b3d15660ad5275ac12902a4f6e84c2fe12484d91
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-01-14 22:27:20 +0100

    x

--- public/index.js
+++ public/index.js
@@ -35,7 +35,8 @@
       if (hours   < 10) {hours   = "0"+hours;}
       if (minutes < 10) {minutes = "0"+minutes;}
       if (seconds < 10) {seconds = "0"+seconds;}
-      return hours+':'+minutes+':'+seconds+'.'+ds;
+      //return hours+':'+minutes+':'+seconds+'.'+ds;
+      return hours+':'+minutes+':'+seconds;
     }
 
     update() {
